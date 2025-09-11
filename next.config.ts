4b95694ee91c72a2572db9f0942e97de21e9516c
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2025-09-11 09:03:42 -0300

    ignoring workspace in the nextjs build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Adiciona uma regra para ignorar todos os arquivos dentro de uma pasta 'codigos_descartados'
+    config.module.rules.push({
+      test: /codigos_descartados\//,
+      loader: 'ignore-loader',
+    });
+    // Adiciona uma regra para ignorar todos os arquivos dentro de uma pasta 'packages'
+    config.module.rules.push({
+      test: /packages\//,
+      loader: 'ignore-loader',
+    });
+
+    return config;
+  },
 };
+
 
 export default nextConfig;
