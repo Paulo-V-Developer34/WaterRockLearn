be85606e944a3538774ba093b0eb3c23153f38f6
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2025-09-18 23:13:21 -0300

    add types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,23 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type Message = {
+  status: string,
+}
+
+export type MessageLogin = Message & {
+  content: User
+}
+
+export type FormState = {
+    message: string,
+    errors?: {
+        userName: string[]
+    }
+}
+
+export type CreateUser = {
+    name: string,
+    password: string
+}
