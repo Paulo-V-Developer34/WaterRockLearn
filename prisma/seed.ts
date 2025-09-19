b8611596620c4dee194a73ed79df80aa5a0b65e3
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2025-09-18 22:46:29 -0300

    crypted password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,33 @@
 import { PrismaClient, Prisma } from "../app/generated/prisma";
+import bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [//caso o tipo não esteja aparecendo é porque a migration ainda não foi executada
-    {
-        email: "jhondoe@gmail.com",
-        name: "Jhon Doe",
-        password: "WRL123@@"
-    },
-    {
-        email: "jhonnydoe@gmail.com",
-        name: "Jhonny Doe",
-        password: "WRL123@@"
-    }
-]
+
+
+const userData = async (): Promise<Prisma.UserCreateInput[]> => {
+    const saltrounds = 10
+    const password = await bcrypt.hash("wrl123@@",saltrounds)
+
+    return [//caso o tipo não esteja aparecendo é porque a migration ainda não foi executada
+        {
+            email: "jhondoe@gmail.com",
+            name: "Jhon Doe",
+            password: password
+        },
+        {
+            email: "jhonnydoe@gmail.com",
+            name: "Jhonny Doe",
+            password: password
+        }
+    ]
+}
 
 //fazendo o processo de seed
 export async function main() {
-    for (const u of userData){
+    const users = await userData()
+
+    for (const u of users){
         await prisma.user.create({data: u})
     }
 }
